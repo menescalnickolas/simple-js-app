d6f8f4928d23ba947b27a3e87f7196487464220d
menescalnickolas <menescalnickolas@gmail.com>
2024-05-11 19:52:48 +0200

    Formatted document
    I formatted the document to include the proper indentation.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 let pokemonList = [
-    {name: "Ponyta",
-    height: 1,
-    types: [fire]
+    {
+        name: "Ponyta",
+        height: 1,
+        types: [fire]
     },
 
-    {name: "Rapidash",
-    height: 1.7,
-    types: [fire]
+    {
+        name: "Rapidash",
+        height: 1.7,
+        types: [fire]
     },
 
-    {name: "Butterfree",
-    height: 1.1,
-    types: [bug, flying]
+    {
+        name: "Butterfree",
+        height: 1.1,
+        types: [bug, flying]
     }
-
-
 ];
