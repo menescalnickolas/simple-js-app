c9c705c8cd42e89c481d86100905ed479ae55b3a
menescalnickolas <menescalnickolas@gmail.com>
2024-05-15 18:40:04 +0200

    Updated for loop with a forEach function

--- js/script.js
+++ js/script.js
@@ -19,11 +19,33 @@ let pokemonList = [
     }
 ];
 
-for (let i = 0; i < pokemonList.length; i++) {
+function myLoopFunction(pokemonList) {
+document.write(`<p class="pokemonList"> Name: ${pokemonList.name} (Height: ${pokemonList.height}) (Type: ${pokemonList.types})</p><br>`);
+}
+
+pokemonList.forEach(myLoopFunction);
+
+
+
+
+
+
+
+
+
+
+/*
+pokemonList.forEach(function (pokemon) {
+    return pokemonList.name + pokemonList.height + pokemonList.types;
+});
+
+document.write(pokemonList.forEach());
+
+ for (let i = 0; i < pokemonList.length; i++) {
 
     if (pokemonList[i].height > 1.5) {
         document.write(`<p class="pokemonList"> Name: ${pokemonList[i].name} (Height: ${pokemonList[i].height}m) - Wow! That's big.</p><br>`);
     } else {
         document.write(`<p class="pokemonList">Name: ${pokemonList[i].name} (Height: ${pokemonList[i].height}m)</p><br>`);
     }
-}
+} */
