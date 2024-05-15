a84470220b1ea4bd14f2d59e15a91b287492e77b
menescalnickolas <menescalnickolas@gmail.com>
2024-05-15 19:18:28 +0200

    First attempt at Part 2 of Task 1.5

--- js/script.js
+++ js/script.js
@@ -1,51 +1,43 @@
 
-let pokemonList = [
-    {
-        name: "Ponyta",
-        height: 1,
-        types: ["Fire"]
-    },
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: "Ponyta",
+            height: 1,
+            types: ["Fire"]
+        },
 
-    {
-        name: "Rapidash",
-        height: 1.7,
-        types: ["Fire"]
-    },
+        {
+            name: "Rapidash",
+            height: 1.7,
+            types: ["Fire"]
+        },
 
-    {
-        name: "Butterfree",
-        height: 1.1,
-        types: ["Bug", " Flying"]
+        {
+            name: "Butterfree",
+            height: 1.1,
+            types: ["Bug", " Flying"]
+        }
+    ]
+
+    function getAll() {
+        return pokemonList;
     }
-];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    }
+})();
 
 function myLoopFunction(pokemonList) {
-document.write(`<p class="pokemonList"> Name: ${pokemonList.name} (Height: ${pokemonList.height}) (Type: ${pokemonList.types})</p><br>`);
+    document.write(`<p class="pokemonList"> Name: ${pokemonList.name} (Height: ${pokemonList.height}) (Type: ${pokemonList.types})</p><br>`);
 }
 
-pokemonList.forEach(myLoopFunction);
+pokemonRepository.getAll(myLoopFunction);
 
 
-
-
-
-
-
-
-
-
-/*
-pokemonList.forEach(function (pokemon) {
-    return pokemonList.name + pokemonList.height + pokemonList.types;
-});
-
-document.write(pokemonList.forEach());
-
- for (let i = 0; i < pokemonList.length; i++) {
-
-    if (pokemonList[i].height > 1.5) {
-        document.write(`<p class="pokemonList"> Name: ${pokemonList[i].name} (Height: ${pokemonList[i].height}m) - Wow! That's big.</p><br>`);
-    } else {
-        document.write(`<p class="pokemonList">Name: ${pokemonList[i].name} (Height: ${pokemonList[i].height}m)</p><br>`);
-    }
-} */
